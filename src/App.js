243a157293b4d251f15af34b5291c2f625cdca7b
Abdelrahman <project-email@example.com>
2026-03-12 17:31:23 +0200

    work on

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./image/logo-light.svg";
 
+// let logoLight = require("../image/logo-light.svg").default;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <img src="./image/logo-light.svg" alt="Logo" />
+        <a href="#"></a>
+      </div>
     </div>
   );
 }
