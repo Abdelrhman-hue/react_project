f57d1430c3800ca49ef8d6a19722e73fa0be54f8
Abdelrahman <project-email@example.com>
2026-03-15 03:08:53 +0200

    lol

---
+++ src/SreachAndCategories.js
@@ -0,0 +1,18 @@
+const SearchAndCategories = () => {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="Search..."  />
+        <h2>Categories</h2>
+        <ul>
+          <li><a href="#">blog</a></li>
+          <li><a href="#">events</a></li>
+          <li><a href="#">Grid</a></li>
+          <li><a href="#">news</a></li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default SearchAndCategories;
